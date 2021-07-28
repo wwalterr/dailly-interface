fd9763a55476cdb8156d9b2f0f845a3ef90eb35d
Hydroxion <whenrique26@gmail.com>
2021-07-28 15:41:52 -0300

    Migrate from raw static files to Jinja 2

--- source/application.py
+++ source/application.py
@@ -1,20 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from fastapi.middleware.gzip import GZipMiddleware
 
 from fastapi.staticfiles import StaticFiles
 
-from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+from fastapi.responses import HTMLResponse
 
 
 application = FastAPI()
 
+application.mount('/static', StaticFiles(directory='static'), name='static')
+
 application.add_middleware(GZipMiddleware, minimum_size=1000)
 
-application.mount(
-    '/public', StaticFiles(directory='public', html=True), name='public')
+templates = Jinja2Templates(directory='templates')
 
 
-@application.get('/', tags=['Index'])
-async def index():
-    return RedirectResponse('/public/index.html', status_code=301)
+@application.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
