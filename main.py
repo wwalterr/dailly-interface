3585cd6b083004facada50a507c131425dac8d81
Hydroxion <whenrique26@gmail.com>
2021-07-28 17:09:15 -0300

    Fix local host conflict

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uvicorn
 if __name__ == '__main__':
     uvicorn.run(
         'source.application:application',
-        host='127.0.0.1',
+        host='0.0.0.0',
         port=4000,
         reload=True,
     )
