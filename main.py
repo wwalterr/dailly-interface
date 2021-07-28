05770147d7688c2c44b4c28574883c81cc55959e
Hydroxion <whenrique26@gmail.com>
2021-07-28 13:13:18 -0300

    License

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import uvicorn
 
 
 if __name__ == '__main__':
-	uvicorn.run(
-		'source.application:application',
-		host='127.0.0.1',
-		port=4000,
-		workers=1,
-		interface='asgi3',
-		reload=True,
-	)
+    uvicorn.run(
+        'source.application:application',
+        host='127.0.0.1',
+        port=4000,
+        workers=1,
+        interface='asgi3',
+        reload=True,
+    )
